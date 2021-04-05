472d7cc0ed3e4453222d940075213d5c9d649d8a
Sflashy <sflashy@mail.com>
2021-04-05 16:22:41 +0300

    example project

---
+++ Assets/KillPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collieded");
+        SceneManager.LoadScene("Main");
+    }
+}
